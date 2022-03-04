4407b8df47ccd00f072c1b084403dfd787e44ac2
bhuwan <bkspoudel@gmail.com>
2022-03-04 10:33:57 +0100

    displayed even or odd info according to the count displayed

--- src/UseStateHook.js
+++ src/UseStateHook.js
@@ -2,13 +2,33 @@ import React, { useState } from "react";
 
 function UseStateHook() {
   const [count, setCount] = useState(0);
+  const [showText, setShowText] = useState(true);
   return (
     <div>
       <h1>Your count</h1>
       <p>{count}</p>
       <div>
-        <button onClick={() => setCount(count + 1)}>Add</button>
-        <button onClick={() => setCount(count - 1)}>Minus</button>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+            setShowText(!showText);
+          }}
+        >
+          Add
+        </button>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+            setShowText(!showText);
+          }}
+        >
+          Minus
+        </button>
+        {showText ? (
+          <p>This is an even number</p>
+        ) : (
+          <p>This is an odd number</p>
+        )}
       </div>
     </div>
   );
