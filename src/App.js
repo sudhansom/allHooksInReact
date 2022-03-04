26e0010e5c0e094090f47be7a2eb98d2ceaf875f
bhuwan <bkspoudel@gmail.com>
2022-03-04 10:20:20 +0100

    imported useState and used it for a counter state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <p>Hello world...</p>
+      <h1>Your count</h1>
+      <p>{count}</p>
     </div>
   );
 }
