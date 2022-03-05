6b47f938e2b9dc83ac2969e79252031753b802a1
bhuwan <bkspoudel@gmail.com>
2022-03-05 22:03:15 +0100

    implemented all the routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import UseEffectHook from "./UseEffectHook";
+import UseReducerHook from "./UseReducerHook";
+import UseStateHook from "./UseStateHook";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<UseEffectHook />}></Route>
+        <Route path="/effect" element={<UseEffectHook />}></Route>
+        <Route path="/state" element={<UseStateHook />}></Route>
+        <Route path="/reducer" element={<UseReducerHook />}></Route>
       </Routes>
     </div>
   );
