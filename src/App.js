fb7e4dbd8e5e68efbe7443bc8e5fd05cf4923014
bhuwan <bkspoudel@gmail.com>
2022-03-05 22:07:32 +0100

    added a homepage to display the urls

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import UseEffectHook from "./UseEffectHook";
 import UseReducerHook from "./UseReducerHook";
 import UseStateHook from "./UseStateHook";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/effect" element={<UseEffectHook />}></Route>
         <Route path="/state" element={<UseStateHook />}></Route>
         <Route path="/reducer" element={<UseReducerHook />}></Route>
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <p>Reducer Hook : -- http://localhost:3000/reducer</p>
+      <p>State Hook : -- http://localhost:3000/state</p>
+      <p>Effect Hook : -- http://localhost:3000/effect</p>
+    </div>
+  );
+}
+
+export default Home;
